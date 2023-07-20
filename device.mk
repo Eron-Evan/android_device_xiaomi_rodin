230aaf70e99ca65e46605c31d93edcb695f086d7
Bruno Martins <bgcngm@gmail.com>
2023-07-20 14:33:30 +0100

    rodin: overlay: Enable smooth display with 120Hz
    There's no point to keep using custom pref to set minimum refresh rate
    when AOSP smooth display feature behaves in the exact same way in
    our case.
    
    Change-Id: I497918a9c9687443a9438fb6291769f6e51d9379
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -244,6 +244,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworksResOverlayRodin \
+    SettingsResOverlayRodin \
     SystemUIOverlayRodin \
     TetheringConfigOverlay \
     WifiOverlay
