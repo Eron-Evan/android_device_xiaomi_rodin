19cd48fa4a8e2736d9734cb5dfa66f1c9a1cdc66
Arian <arian.kulmer@web.de>
2024-05-18 15:52:45 +0200

    rodin: Provide a static lib for power mode extension
    Change-Id: I3e2cf17bf1e733838e9e18afdf3f888e814593dc
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -116,7 +116,7 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_BOARD_PLATFORM := mt6899
 
 # Power
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-xiaomi
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
