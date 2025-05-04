7cfcd0e00e0b6a194b8bf52371ed054d7bdd06c4
claxten10 <claxten10@gmail.com>
2025-05-04 22:12:13 +0000

    rodin: Uprev sensors-V2-ndk to V3-ndk
    Change-Id: I8b0b337ad43970c2482232345f5cd16197e712c2
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,26 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee': blob_fixup()
         .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V4-ndk.so'),
+    (
+        'vendor/bin/mnld',
+        'vendor/lib64/libpqconfig.so',
+        'vendor/lib64/libaalservice.so',
+        'odm/lib64/hw/displayfeature.default.so',
+        'odm/lib64/libpaperMode.so',
+        'odm/lib64/libmiBrightness.so',
+        'odm/lib64/libmiSensorCtrl.so',
+        'odm/lib64/libcolortempmode.so',
+        'odm/lib64/libtruetone.so',
+        'odm/lib64/libsre.so',
+        'odm/lib64/libsdr2hdr.so',
+        'odm/lib64/libdither.so',
+        'odm/lib64/libhistprocess.so',
+        'odm/lib64/libadaptivehdr.so',
+        'odm/lib64/librhytheyecare.so',
+        'odm/lib64/libflatmode.so',
+        'odm/lib64/libvideomode.so',
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
