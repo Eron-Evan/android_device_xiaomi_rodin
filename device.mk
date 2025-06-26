0c88d7b87571e3f1c69493f9819bcad7f0fcbdff
claxten10 <claxten10@gmail.com>
2025-06-26 18:43:10 +0000

    rodin: Inherit common MTK IMS product
    Change-Id: I565237cc552a59000a72faeb93156e7392955928
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Inherit common MediaTek IMS
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.batterysecret.rc \
