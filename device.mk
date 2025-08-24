2672b674bf1da18179c0735603a56a72b945aa90
Ido <dev.xyzuniverse@gmail.com>
2025-08-24 18:35:53 +0200

    rodin: Switch to AIDL BT
    Change-Id: I2753d9fb4e6ee0fcb5e05e43cfa1da792b5e9c88
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -119,7 +119,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-mtk-service
+    android.hardware.bluetooth-service.default.mtk
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
