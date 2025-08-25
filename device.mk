7cdec13f8249e428c2680fa585acdce902e61e10
Ido <dev.xyzuniverse@gmail.com>
2025-08-25 20:29:03 +0200

    rodin: Initial overlays - Partially taken from duchamp and tanzanite.
    Change-Id: Iec2406c14f3528f6ca93af1ad900164d55932755
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -239,6 +239,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/com.nxp.mifare.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlayRodin \
+    SystemUIOverlayRodin
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.pixel-libperfmgr \
