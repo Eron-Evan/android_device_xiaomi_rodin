fe495c49c4f8afca21ae965b99aea82a423766f4
xyzuniverse <dev.xyzuniverse@gmail.com>
2026-03-08 18:43:14 +0000

    rodin: Something is blooming...
    Change-Id: Ib6e8c8d1e4f89cdef41a44ae48639f02d4f8c647
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- halcyon_rodin.mk
+++ halcyon_rodin.mk
@@ -8,14 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common halcyon stuff.
+$(call inherit-product, vendor/halcyon/config/common.mk)
+HALCYON_BUILD_TYPE := Beta
+WITH_GMS := true
 
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
 
 PRODUCT_DEVICE := rodin
-PRODUCT_NAME := lineage_rodin
+PRODUCT_NAME := halcyon_rodin
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 2412DPC0AG
 PRODUCT_MANUFACTURER := xiaomi
