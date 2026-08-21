04cce87356cf3fa246a2ccb2c707935a23a4f817
Eron Evan <eronevan007@gmail.com>
2026-08-21 10:42:51 +0000

    InfinityX Bringup
    Signed-off-by: Eron Evan <eronevan007@gmail.com>

--- infinity_rodin.mk
+++ infinity_rodin.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
@@ -50,8 +50,16 @@ TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_SHIPS_BCR := true
 TARGET_SUPPORTS_GOOGLE_TELEPHONY := false
 TARGET_OPTIMIZED_DEXOPT := true
-WITH_GMS := true
 WITH_BCR := true
 TARGET_CUSTOM_UDFPS := true
 WITH_GMS_COMMS_SUITE := true
 TARGET_ENABLE_BLUR := true
+
+# Infinity-X Specific Flags
+INFINITY_MAINTAINER := ERON
+WITH_GAPPS := true
+USE_MOTO_CALCULATOR := true
+TARGET_BUILD_VIMUSIC := true
+
+# Inherit some extras stuff
+$(call inherit-product-if-exists, vendor/extras/extras.mk)
